54171839be838392c83c4a778e340abfd38d2e51
holajoa <jiuming.qin19@imperial.ac.uk>
2021-06-05 20:25:50 +0800

    Update K estimation

--- cluster_models.py
+++ cluster_models.py
@@ -1,16 +1,60 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.spatial.distance import squareform, pdist
+
+### Functions for estimating K and L function ###
+
+def k_estimate(x, y, window=[0, 1, 0, 1], n=200, r_max=None):
+    points = np.column_stack((x, y))
+    x_min, x_max, y_min, y_max = window
+    area = (y_max-y_min)*(x_max-x_min)
+    N = len(points)
+    lam_hat = N/area
+    
+    u = squareform(pdist(points)) + np.eye(N)
+    d1 = np.minimum(x - x_min, x_max - x)
+    d2 = np.minimum(y - y_min, y_max - y)
+    d1 = np.tile(d1, (N, 1))
+    d2 = np.tile(d2, (N, 1))
+    d_hypot = np.hypot(d1, d2)
+    
+    w1 = 1 - 1/np.pi*(np.arccos(np.minimum(d1, u)/u) + np.arccos(np.minimum(d2, u)/u))
+    uu = u.copy()
+    uu[uu < d_hypot] = d_hypot[uu < d_hypot]
+    w2 = 3/4 - 1/(2*np.pi)*(np.arccos(d1/uu) + np.arccos(d2/uu))
+    
+    d_hypot = np.hypot(d1, d2)
+    w_ind = u <= d_hypot
+    w = w_ind*w1 + ~w_ind*w2
+    u -= np.eye(N)
+    
+    if not r_max:
+        r_max = min(y_max-y_min, x_max-x_min)/2
+    
+    r = np.linspace(0, r_max, n)
+    k = np.zeros(n)
+    for i in range(n):
+        d_ind = (u < r[i]) & (u > 0)
+        k[i] = np.sum((d_ind/w)/N/lam_hat)
+    
+    return r, k
 
 
-### Thomas process ###
+def l_estimate(x, y, **kwargs):
+    r, k = k_estimate(x, y, **kwargs)
+    l = np.sqrt(k/np.pi)
+    return r, l
+
+
+
+### Thomas process VS CSR ###
 
 def thomas_cluster_process(
         lambda_parent=10, 
         lambda_daughter=100, 
         radius_cluster=.1, 
         daughter_cov=1, 
-        boundaries=[-0.5, 0.5, -0.5, 0.5], 
-        plot=True
+        boundaries=[-0.5, 0.5, -0.5, 0.5]
     ):
     
     """
@@ -72,35 +116,53 @@ def thomas_cluster_process(
     bool_inside = ((xx >= xmin) & (xx <= xmax) & (yy >= ymin) & (yy <= ymax))
     xx = xx[bool_inside]
     yy = yy[bool_inside]
-
-    # K function implementation
-    T = np.linspace(0, np.sqrt((xmax-xmin)**2 + (ymax-ymin)**2), 100)
-    K = np.pi * T**2 + (1/lambda_parent) * (1 - np.exp(-T**2 / (4*daughter_cov)))
-    K_ = (1/lambda_parent) * (1 - np.exp(-T**2 / (4*daughter_cov)))
     
-    if plot:
-        plt.figure(figsize=(6, 6))
-        plt.title('Thomas process', fontsize=14)
-        plt.scatter(xx, yy, alpha=0.4)
-        plt.xlabel('x')
-        plt.ylabel('y')
-        plt.axis('equal')
-        plt.show()
-        
-        #plt.subplot(122)
-        #plt.title(r'$K(t) - \pi t^2$')
-        #plt.plot(T, K_, 'r')
-        #plt.legend()
+    return np.c_[xx, yy]
 
-    return np.c_[xx, yy]#, T, K
+def csr(lam, window=[0, 1, 0, 1]):
+    x_min, x_max, y_min, y_max = window
+    area = (y_max-y_min) * (x_max-x_min)
+    N = np.random.poisson(lam * area)
+    x_list = np.random.uniform(x_min, x_max, N)
+    y_list = np.random.uniform(y_min, y_max, N)
+    return np.c_[x_list, y_list]
 
-np.random.seed(7)
+# Plotting
+np.random.seed(77)
+boundaries = [-1, 1, -1, 1]
+
 tcp = thomas_cluster_process(
         lambda_parent=10, 
         lambda_daughter=50, 
         radius_cluster=.2, 
         daughter_cov=.2, 
-        boundaries=[-1, 1, -1, 1], 
-        plot=True
+        boundaries=boundaries, 
     )
 
+hpp = csr(lam=tcp.shape[0]/4, window=[-1.2, 1.2, -1.2, 1.2])
+hpp = np.array([s for s in hpp if (s[0]<=1 and s[0]>=-1 and s[1]<=1 and s[1]>=-1)])
+
+plt.figure(figsize=(23, 6))
+plt.subplot(131)
+plt.title('Thomas Process')
+plt.scatter(tcp[:, 0], tcp[:, 1], alpha=0.4)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.axis('equal')
+
+plt.subplot(132)
+plt.scatter(hpp[:, 0], hpp[:, 1], alpha=0.4)
+plt.title('CSR')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.axis('equal')
+
+plt.subplot(133)
+r, k = k_estimate(tcp[:, 0], tcp[:, 1], window=boundaries) 
+r0, k0 = k_estimate(hpp[:, 0], hpp[:, 1], window=boundaries) 
+plt.plot(r, k-np.pi*r**2, 'r-', label=r'$\hat{K}}_{thom}-\pi r^2$') 
+plt.plot(r0, k0-np.pi*r0**2, 'b-', label=r'$\hat{K}_{csr}-\pi r^2$') 
+
+plt.plot(r, r*0, 'k--')
+plt.legend()
+plt.show()
